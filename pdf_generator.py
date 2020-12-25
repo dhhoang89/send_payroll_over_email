81735c40f0488bd4dca5617cbe0a93d4be91dbd1
dhhoang89 <danghuyhoang18@gmail.com>
2020-12-25 16:41:00 +0700

    Content Update

--- pdf_generator.py
+++ pdf_generator.py
@@ -2,6 +2,8 @@ from xhtml2pdf import pisa
 from os.path import join, dirname, abspath 
 import jinja2
 import xlrd
+import locale
+from datetime import datetime
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
 TEMPLATE_FILE = "payslip_template.html"
@@ -12,17 +14,55 @@ loc = join(dirname(dirname(abspath(__file__))), 'send_payroll_over_email' ,'data
 wb=xlrd.open_workbook(loc)
 sheet_names = wb.sheet_names()
 sheet=wb.sheet_by_index(0)
+locale.setlocale( locale.LC_ALL, '' )
 for i in range(1,sheet.nrows):
     body = {
         "data":{
             "Name": sheet.cell_value(i,1),
-            "Salary": sheet.cell_value(i,2),
+            "Code_fm": sheet.cell_value(i,0),
+            "days_contract": sheet.cell_value(i,3),
+            "days_probation": sheet.cell_value(i,4),
+            "Auto_Working_day": sheet.cell_value(i,5),
+            "Working_day": sheet.cell_value(i,6),
+            "Full_paid_leave": sheet.cell_value(i,7),
+            "Special_leave": sheet.cell_value(i,8),
+            "Unpaid_leave": sheet.cell_value(i,9),
+            "Sick_leave": sheet.cell_value(i,10),
+            "Maternity_leave": sheet.cell_value(i,11),
+            "Gross_Salary_level_fm": f"{int(sheet.cell_value(i,12)):,}",
+            "Probation_salary_fm": f"{int(sheet.cell_value(i,13)):,}",
+            "Actual_sal_contract": f"{int(sheet.cell_value(i,14)):,}",
+            "Actual_pro_sal": f"{int(sheet.cell_value(i,15)):,}",
+            "Gross_salary_fm": f"{int(sheet.cell_value(i,16)):,}",
+            "Total_additional_fm": sheet.cell_value(i,17),
+            "Total_OT_hour": sheet.cell_value(i,18),
+            "Total_OT_fm": f"{int(sheet.cell_value(i,19)):,}",
+            "Lunch_allowance_fm": f"{int(sheet.cell_value(i,20)):,}",
+            "Relotation_support__package_fm": f"{int(sheet.cell_value(i,21)):,}",
+            "Referral_bonus_fm": f"{int(sheet.cell_value(i,22)):,}",
+            "Remedy_fm": f"{int(sheet.cell_value(i,23)):,}",
+            "Total_deduction_fm": f"{int(sheet.cell_value(i,24)):,}",
+            "SI_by_employee": f"{int(sheet.cell_value(i,25)):,}",
+            "HI_by_employee": f"{int(sheet.cell_value(i,26)):,}",
+            "UI_by_employee": f"{int(sheet.cell_value(i,27)):,}",
+            "Total_SI_by_employee_fm": f"{int(sheet.cell_value(i,28)):,}",
+            "Late_time": sheet.cell_value(i,29),
+            "Late_time_fm": f"{int(sheet.cell_value(i,30)):,}",
+            "Early_time": sheet.cell_value(i,31),
+            "Early_time_fm": f"{int(sheet.cell_value(i,32)):,}",
+            "Dependent_Qty": sheet.cell_value(i,33),
+            "Taxable_income_fm": f"{int(sheet.cell_value(i,34)):,}",
+            "Income_tax_fm": f"{int(sheet.cell_value(i,35)):,}",
+            "Other__deduction_fm": f"{int(sheet.cell_value(i,36)):,}",
+            "net_taken_home_fm": f"{int(sheet.cell_value(i,37)):,}",
+            "from_date": sheet.cell_value(i,38),
+            "to_date": sheet.cell_value(i,39)
         }
     }
 
     # This renders template with dynamic data 
     sourceHtml = template.render(json_data=body["data"]) 
-    outputFilename = "payslip"+str(sheet.cell_value(i,0))+".pdf"
+    outputFilename = datetime.strptime(sheet.cell_value(i,39),'%d/%m/%Y').strftime('%Y%m')+" - CoAsia SEMI Vietnam - Payslip - "+sheet.cell_value(i,0)+".pdf"
     # Utility function
     def convertHtmlToPdf(sourceHtml, outputFilename):
         # open output file for writing (truncated binary)
@@ -37,7 +77,7 @@ for i in range(1,sheet.nrows):
         resultFile.close()
 
         # return True on success and False on errors
-        print(pisaStatus.err, type(pisaStatus.err))
+        print("Generated payslip for",sheet.cell_value(i,1))
         return pisaStatus.err
 
     if __name__ == "__main__":
--- send_payroll_over_email.py
+++ send_payroll_over_email.py
@@ -4,6 +4,7 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.application import MIMEApplication
 from os.path import join, dirname, abspath
+from datetime import datetime
 import xlrd
 def send_mail():
     sender_email = "danghuyhoang18@gmail.com"
@@ -13,14 +14,14 @@ def send_mail():
     sheet=wb.sheet_by_index(0)
     for i in range(1,sheet.nrows):
         text = MIMEMultipart('alternative')
-        text.attach(MIMEText("<html><head></head><body>Hi "+sheet.cell_value(i,1)+",<br>I would like to send your payslip.<br> Thank you for your contribution this month.</body></html>", "html", _charset="utf-8"))
+        text.attach(MIMEText("<html><head></head><body>Hi "+sheet.cell_value(i,1)+",<br>On behalf of CoAsia SEMI Vietnam, we would like to send you the payslip of your salary in " + datetime.strptime(sheet.cell_value(i,39),'%d/%m/%Y').strftime('%m-%Y') +  ", please kindly check attached file herewith for more details. <br>Should you have any queries, don't hesitate to let us know. <br>Thank you for your efforts during the month. <br>Best Regards,</body></html>", "html", _charset="utf-8"))
         msg = MIMEMultipart('mixed')
         msg.attach(text)
-        msg['Subject'] = 'Luong nhan vien '+ sheet.cell_value(i,1)
+        msg['Subject'] = 'Payslip for period from '+ sheet.cell_value(i,38)+ ' to ' + sheet.cell_value(i,39)
         msg['From'] = sender_email
-        msg['To'] = "danghuyhoang18@gmail.com"
-        pdf = MIMEApplication(open("./payslip_files/payslip00"+str(i)+".pdf", 'rb').read())
-        pdf.add_header('Content-Disposition', 'attachment', filename= "payslip00"+str(i)+".pdf")
+        msg['To'] = sheet.cell_value(i,2)
+        pdf = MIMEApplication(open("./payslip_files/"+datetime.strptime(sheet.cell_value(i,39),'%d/%m/%Y').strftime('%Y%m')+" - CoAsia SEMI Vietnam - Payslip - "+sheet.cell_value(i,0)+".pdf", 'rb').read())
+        pdf.add_header('Content-Disposition', 'attachment', filename= datetime.strptime(sheet.cell_value(i,39),'%d/%m/%Y').strftime('%Y%m')+" - CoAsia SEMI Vietnam - Payslip - "+sheet.cell_value(i,0)+".pdf")
         msg.attach(pdf) 
 
         try:
@@ -28,8 +29,8 @@ def send_mail():
                 smtpObj.ehlo()
                 smtpObj.starttls()
                 smtpObj.login("danghuyhoang18@gmail.com", "cuzztiiarsglzfda")
-                smtpObj.sendmail(sender_email, 'danghuyhoang18@gmail.com', msg.as_string())
-                print("Sent to "+sheet.cell_value(i,3)+" successfully")
+                smtpObj.sendmail(sender_email, sheet.cell_value(i,2), msg.as_string())
+                print("Sent to "+sheet.cell_value(i,2)+" successfully")
         except Exception as e:
             print(e)
 
